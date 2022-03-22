16ff06b33394a127bacdf54b0c06ac67816e3cc6
heloisanascimento <heloisa.nascimento@outlook.como>
2022-03-21 21:53:38 -0300

    commit

---
+++ JavaScript/nome.js
@@ -0,0 +1,27 @@
+/*console.log('heloisa');
+var email = "heloisa.nascimento@outlook.com"
+var telefone = 910297978
+console.log( "Meu email é "+ email + " " + "meu telefone é " + telefone ); 
+*/ 
+
+/*var n1 =5;
+var n2 =8;
+var n3 =4;
+var n4 =10;
+var media = 4;
+var total;
+
+/*total = n1 + n2;
+total = n1 - n2;
+total = n1 * n2;
+total = n1 / n2;
+total = n1 % n2;
+*/
+/*total = (n1+n2+n3+n4)/media;
+if (total >= 7){
+ console.log("aprovado" + total);
+} else if (total >= 5){
+     console.log("recuperaçao " + total);
+} else{
+ console.log("reprovado "+ total);
+}*/
---
+++ JavaScript/par.js
@@ -0,0 +1,10 @@
+var alunos = 14;
+for (let x = 0; x <= alunos; x++) {
+    if (x == 0) {
+        console.log("numero zero");
+    } else if (x == 0) {
+        console.log("numero é par: " + x);
+    } else {
+        console.log("o numero è impar" + x)
+    }
+}
